240d03d7f4c30859d2920880b07728e83065e583
hyunbeak <38645337+hyunbeak@users.noreply.github.com>
2019-04-26 17:28:06 +0900

    확인되지 않는 도메인 개선

--- src/util/NSLookup.java
+++ src/util/NSLookup.java
@@ -10,19 +10,24 @@ import java.net.UnknownHostException;
 public class NSLookup {
 
 	public static void main(String[] args) {
+		NSLookup NSL = new NSLookup();
+		NSL.domainToIp();
+	}
+
+	
+	public void domainToIp() {
+
 		BufferedReader br = null;
 		System.out.print("> ");
 		try {
 			InputStreamReader isr = new InputStreamReader(System.in,"UTF-8");
 			br =new BufferedReader(isr);
 			String line = null;
-			while((line = br.readLine()) != null) {
-				
+			while((line = br.readLine()) != null) {				
 				if(line.equals("exit")) {
 					System.out.println("종료~");
 					break;
 				}
-
 				InetAddress[] inetAddress = InetAddress.getAllByName(line);
 				for(InetAddress ia : inetAddress) {
 					System.out.println( line + ": " + ia.getHostAddress());
@@ -32,8 +37,9 @@ public class NSLookup {
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			// e.printStackTrace();
 			System.out.println(e.getLocalizedMessage() +" 존재하지 않는 도메인 입니다");
+			domainToIp();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -42,12 +48,8 @@ public class NSLookup {
 					br.close();					
 				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-
-
 	}
-
 }
